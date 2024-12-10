62dfb52545c691aebfbe3c89838c1496e18ffdea
fethi <fethiguneyy@gmail.com>
2024-12-10 14:35:03 +0300

    day 5 template literals added

---
+++ day05/Template Literals.js
@@ -0,0 +1,20 @@
+function sides(literals, ...expressions) {
+    
+    let area = expressions[0];
+    let perimeter =expressions[1];
+    
+    let s1= (perimeter+Math.sqrt(perimeter * perimeter - (16*area))) / 4;
+    
+    let s2 = (perimeter-Math.sqrt(perimeter * perimeter - (16*area)))/4;
+    
+    let array = [s1, s2];
+    
+    array.sort(function (a, b) {
+        return a - b;
+        });
+    
+    return array;
+    
+    
+    
+}
